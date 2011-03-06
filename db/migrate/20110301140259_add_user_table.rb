0cb9bcbfbfc4f6f7e50d5e9cfcd2ead89798d6cb
git-cbrewer <cbrewer@patientslikeme.com>
2011-03-06 15:07:54 -0500

    Add account model + controller

--- db/migrate/20110301140259_add_user_table.rb
+++ db/migrate/20110301140259_add_user_table.rb
@@ -1,15 +1,25 @@
 class AddUserTable < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :name
-      t.string :hashed_password
-      t.string :salt
-
+      t.string    :name, :optional => false
+      t.string    :display_name, :optional => false
+      t.boolean   :deleted, :default => false
+      t.string    :hashed_password, :optional => false
+      t.string    :salt, :optional => false
       t.timestamps
-    end      
-  end
+    end
     
+    create_table :services do |t|
+      t.integer   :user_id, :optional => false
+      t.integer   :service_type, :optional => false
+      t.string    :service_uname, :optional => false
+      t.string    :service_pwd, :optional => false
+      t.timestamps
+    end
+  end
+
   def self.down
+    drop_table :services
     drop_table :users
   end
 end
