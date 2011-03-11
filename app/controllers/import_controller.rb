adcaea37e078bc24e79c8254dafdf134e8657123
git-cbrewer <cbrewer@patientslikeme.com>
2011-03-11 02:57:23 -0500

    debugging flickr api on production

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -93,6 +93,8 @@ class ImportController < AuthenticatedController
     token = @service.service_token if @service
     
     a = {}
+    puts "**** #{flickr_config[:key]}"
+    puts "**** #{flickr_config[:secret]}"
     a[:key] = $flickr_config[:key]
     a[:secret] = $flickr_config[:secret]
     a[:token] = token
