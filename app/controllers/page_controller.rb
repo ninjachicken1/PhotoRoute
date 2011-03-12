6bc94b5730cb77044b78f9302368ee368f73e9d0
git-cbrewer <cbrewer@patientslikeme.com>
2011-03-12 14:39:00 -0500

    Add static page handler

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,14 @@
+class PageController < ApplicationController
+  def show
+    
+    respond_to do |format|
+      format.html { 
+        if params[:name]
+          render "#{params[:name].downcase}"
+        else
+          render :status => "404"
+        end
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,18 +18,8 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :users
   end
 
-  map.welcome 'welcome', :controller => 'welcome', :action => 'index', :conditions => { :method => :get }
+  # Unauthenticated pages
+  map.page '/page/:name', :controller => 'page', :action => 'show', :conditions => { :method => :get }
+  map.welcome '/welcome', :controller => 'welcome', :action => 'index', :conditions => { :method => :get }
   map.root :welcome
 end
-
-=begin
-# Logging in (creating a session) - Singleton
-GET     photoroute.net/login          => :new       # form to login
-POST    photoroute.net/login          => :create    # authenticate and login from post info
-GET     photoroute.net/logout         => :destroy   # logout the current session
-
-# Creating a user object - Plural Resources
-GET     photoroute.net/subscribers/new     => :new       # form to create a new user
-POST    photorotue.net/subscribers         => :create    # create a new user from post info
-DELETE  photoroute.net/subscribers/1       => :destroy   # delete a user and all records
-=end
