19f40cd89df62e53102b5b8ff7e87bede1c2c113
git-cbrewer <cbrewer@patientslikeme.com>
2011-03-12 21:14:58 -0500

    Added font mimetypes, some CSS changes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,13 @@ class WelcomeController < ApplicationController
   
   # GET /
   # GET /welcome
-  def index    
+  def index      
     respond_to do |format|
-      format.html { render :template => "welcome/welcome.html.erb", :layout => false }
+      if session[:user_id]
+        format.html { redirect_to paths_path if session[:user_id] }
+      else        
+        format.html { render :template => "welcome/welcome.html.erb", :layout => false }
+      end
     end
   end
   
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,15 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# Was trying to solve this issue in Chrome:
+#   "Resource interpreted as font but transferred with MIME type application/x-woff""
+# Not sure if correct: http://www.jbarker.com/blog/2009/mime-type-css-web-fonts
+fonts = {
+  '.eot' => 'application/vnd.ms-fontobject',
+  '.ttf' => 'font/ttf',
+  '.otf' => 'font/otf',
+  '.woff' => 'application/x-woff'
+}
+Rack::Mime::MIME_TYPES.merge!(fonts)
+
