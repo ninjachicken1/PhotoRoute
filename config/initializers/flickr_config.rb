1b9df4aa2d02492c5dad5d310df12a8264b51957
git-cbrewer <cbrewer@patientslikeme.com>
2011-04-04 22:50:56 -0400

    In production (heroku) support loading flickr api info from ENV vars.

--- config/initializers/flickr_config.rb
+++ config/initializers/flickr_config.rb
@@ -3,26 +3,38 @@ $flickr_config = nil
 
 yml = File.dirname(__FILE__) + '/../flickr.yml'
 error = nil
+config = {}
 begin
-  if File.exist?(yml)
-    config = YAML::load_file(yml)
-
-    # Check required keys
-    if config[:key].nil? || 
-      config[:key].length == 0 || 
-      config[:secret].nil? ||
-      config[:secret].length == 0
-
-      error = "Flickr API requires the fields 'key' and 'secret'. See README for instructions."
+  env = RACK_ENV if defined? RACK_ENV
+  env = RAILS_ENV if defined? RAILS_ENV
+  if ['development'].include?(env)
+    puts "***** DEVELOPMENT MODE: expecting config/flickr.yml"
+    # Load from config file
+    if File.exist?(yml)
+      config = YAML::load_file(yml)
     else
-      $flickr_config = config
+      error = "In DEVELOPMENT, Flickr API requires the file 'config/flickr.yml'. See README for instructions."
     end
-    
   else
-    error = "Flickr API requires the file 'config/flickr.yml'. See README for instructions."
+    puts "***** NON-development MODE: expecting flickr ENV variables."
+    # Load from ENV variables (compatible with Heroku)
+    config[:key] = ENV[:flickr_key]
+    config[:secret] = ENV[:flickr_secret]
   end
+
+  # Check required keys
+  if config[:key].nil? || 
+    config[:key].length == 0 || 
+    config[:secret].nil? ||
+    config[:secret].length == 0
+
+    error = "Flickr API requires the fields 'key' and 'secret'. See README for instructions."
+  else
+    $flickr_config = config
+  end
+
 rescue Exception => ex
-  error = "Flickr API could not be initialized from 'config/flickr.yml'. See README for instructions.\nError: #{ex.message}\n#{ex.backtrace}"
+  error = "Flickr API could not be initialized. See README for instructions.\nError: #{ex.message}\n#{ex.backtrace}"
 end
 
 puts "Flickr API Initialization Error: " + error if error
